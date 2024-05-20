5b466a0d3d32a565971d43fc43e8a0f3762e296b
Daniel Prospero <daniel.ribeiro@elev.tec.br>
2024-05-20 19:58:36 -0300

    implementando o Seeder e Factory.

---
+++ database/seeds/FornecedoresSeede.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FornecedoresSeede extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Instanciando o objeto
+        $fornecedor = new \App\Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'comtato@fornecedor100.com.br'; 
+        $fornecedor->save(); 
+
+        // Utilizando o método create
+        \App\Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor200.com.br' 
+        ]);
+
+        // Insert comum 
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+
+    }
+}
