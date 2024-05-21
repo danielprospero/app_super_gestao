8f83076e31ec6df3839cc96ac25bfe42adcebe40
Daniel Prospero <daniel.ribeiro@elev.tec.br>
2024-05-21 20:54:43 -0300

    Adicionando o objeto Request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
+        //dd($request);
         return view('site.contato', ['titulo' => 'Contato']);
     }
 }
