660c6d0f4d11c19472d50c3189b6589bf52da591
Daniel Prospero <danielprosperoribeiro@outlook.com.br>
2024-05-26 20:22:42 -0300

    Customizando a apresentação dos erros de validação.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -12,19 +12,20 @@ class ContatoController extends Controller
 
         $motivo_contatos = MotivoContato::all();
 
-        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
 
         //realizar a validação dos dados do formulário recebidos no request
         $request->validate([
-            'nome' => 'required|min:3|max:40',
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
-        // SiteContato::create($request->all());
+         SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
