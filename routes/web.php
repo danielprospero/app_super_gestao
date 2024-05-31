e5f7c103d5f95eeaa0a4514968b5e027a330620a
Daniel Prospero <danielprosperoribeiro@outlook.com.br>
2024-05-30 21:30:21 -0300

    Criando o model LogAcesso e sua migration.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\logAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +22,9 @@ use Illuminate\Support\Facades\Route;
 | options
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)->get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::middleware(LogAcessoMiddleware::class)->get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function () { return 'Login'; })->name('site.login');
 
