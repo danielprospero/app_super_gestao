c1bde1c70d3305229b702b394ad2faf30422e03c
Daniel Prospero <danielprosperoribeiro@outlook.com.br>
2024-06-01 19:26:07 -0300

    Implementando o menu de opções da área protegida da aplicação.

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -15,21 +15,15 @@ class AutenticacaoMiddleware
      */
     public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
-        echo  "$metodo_autenticacao <br>";
-
-        if($metodo_autenticacao == 'padrao'){
-            echo 'Autenticação padrão '.$perfil.' <br>';
-        }else if($metodo_autenticacao == 'ldap'){
-            echo 'Autenticação LDAP '.$perfil.' <br>';
-        }
-        if($perfil == 'admin'){
-            echo 'Perfil de administrador <br>';
-        }else if($perfil == 'visitante'){
-            echo 'Perfil de visitante <br>';
-        }
-        if(false){
+      session_start();
+      
+        if(isset($_SESSION['email']) && $_SESSION['email'] != '')
+        {
             return $next($request);
         }
-        return Response('Acesso negado! Rota exige autenticação!');
+        else
+        {
+            return redirect()->route('site.login', ['erro' => 2]);
+        }
     }
 }
