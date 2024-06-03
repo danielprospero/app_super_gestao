489f51818881262d433766c6c7f9094647c84afd
Daniel Prospero <danielprosperoribeiro@outlook.com.br>
2024-06-02 22:22:04 -0300

    Implementando o cadastro de fornecedores CRUD.

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,12 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
 
     public function index()
     {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
     }
+
+    public function listar(Request $request)
+    {
+        $fornecedores = Fornecedor::where('nome', 'like', '%'.$request->input('nome').'%')
+            ->where('site', 'like', '%'.$request->input('site').'%')
+            ->where('uf', 'like', '%'.$request->input('uf').'%')
+            ->where('email', 'like', '%'.$request->input('email').'%')
+            ->paginate(2);
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores, 'request' => $request->all()]);
+    }
+    public function adicionar(Request $request)
+    {
+        $msg = '';
+
+        // Adicionar
+        if($request->input('_token') != '' && $request->input('id') == '') {
+            $regras = [
+                'nome' => 'required|min:3|max:40|unique:fornecedores',
+                'site' => 'required',
+                'uf' => 'required|size:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'nome.required' => 'O campo nome é obrigatório',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'nome.unique' => 'O nome informado já está em uso',
+                'site.required' => 'O campo site é obrigatório',
+                'uf.required' => 'O campo uf é obrigatório',
+                'uf.size' => 'O campo uf deve ter 2 caracteres',
+                'email.email' => 'O campo email deve ser um email válido'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Cadastro realizado com sucesso';
+
+        }
+    
+        //edição
+        if($request->input('_token') != '' && $request->input('id') != '') {
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizado com sucesso';
+            } else {
+                $msg = 'Erro ao tentar atualizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
+
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+    public function editar($id, $msg = '') {
+
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
+    }
+
+    public function excluir($id) {
+
+        Fornecedor::find($id)->delete();
+
+        return redirect()->route('app.fornecedor');
+    }
+
+
+
 }
